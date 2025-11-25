499e43cdc87d3011051c50701a2932489f2361bf
ivanendara-hash <ivanendara@gmail.com>
2025-11-25 10:49:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import fetch from "node-fetch";
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); 
 
 function speak(text, end = true) {
   return {
